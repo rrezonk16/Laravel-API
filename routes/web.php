98cc4d64f48f6da0014528d47d8282af6d7f66fb
Rrezon Krasniqi <rrezonkrasniqi32@gmail.com>
2024-03-09 00:21:45 +0100

    minimal changes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function () {
+    return 'Hello, welcome to my website!';
+});
+
 Route::get('/run-migration', function(){
     Artisan::call('optimize:clear');
     Artisan::call('migrate:fresh --seed');
